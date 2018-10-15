a16f4d5c08130023183764dd831dd47eb4b04143
U-BSD\349973 <349973@SHS-10L74C3F1.beaverton.k12.or.us>
2018-10-15 09:03:28 -0700

    working on voids

--- studentlist.cpp
+++ studentlist.cpp
@@ -10,27 +10,17 @@ struct Student {
   float gpa;
 };
 
+char response[10];
+
 void printStudents(struct Student);
 void addStudent(struct Student);
 void deleteStudent(struct Student);
 void quit(struct Student);
 
-int response = 0;
-
 vector<Student*>* studentlist;
 
-int main() {
-  cout << "Enter ADD, PRINT, DELETE, or QUIT" << endl;
-  cin >> response;
-  if (response = "ADD") {
-  addStudent;
-  cout << student;
-  }
-  if (response = "PRINT") {
-    printStudents;
-  }
-  vector <Student*>* studentlist = new vector<Student*>();
-  void addStudent(struct Student) {
+void addStudent(struct Student) {
+  cout << "Test add" << endl;
   Student student;
   cout << "Enter student first name" << endl;
   cin >> student.firstname;
@@ -40,7 +30,45 @@ int main() {
   cin >> student.id;
   cout << "Enter student GPA" << endl;
   cin >> student.gpa;
+}
+
+int main() {
+  do {
+  cout << "Enter ADD, PRINT, DELETE, or QUIT" << endl;
+  cin >> response;
   }
+  while (response == "ADD"); {
+    cout << "Adding student" << endl;
+    addStudent();
+  }
+  
+  if (response == "PRINT") {
+    cout << "Printing students" << endl;
+    printStudents();
+    }
+  
+   if (response == "DELETE") {
+   cout << "Deleting student" << endl;
+    }
+
+   if (response == "QUIT") {
+     cout << "Quiting" << endl;
+   }
+   vector <Student*>* studentlist = new vector<Student*>();
+}
+//for ADD
+//void addStudent(struct Student) {
+  //cout << "Test Add" << endl;
+  //Student student;
+   //cout << "Enter student first name" << endl;
+   //cin >> student.firstname;
+   //cout << "Enter student last name" << endl;
+   //cin >> student.lastname;
+   //cout << "Enter student ID" << endl;
+   //cin >> student.id;
+   //cout << "Enter student GPA" << endl;
+   // cin >> student.gpa;
+   //}
 
   //for PRINT
   void printStudents(struct Student) {
@@ -48,5 +76,4 @@ int main() {
   cout << student.firstname << " " << student.lastname <<" ID:" << student.id << " GPA:";
   cout << student.gpa << endl;
   }
-  return 0;
-}
+
