f636bf9ca4a852fdfe2bf793186f0c1c636f9a95
U-BSD\349973 <349973@SHS-10L74C3F1.beaverton.k12.or.us>
2018-10-17 08:48:00 -0700

    working on do while loop connecting to voids

--- studentlist.cpp
+++ studentlist.cpp
@@ -10,70 +10,64 @@ struct Student {
   float gpa;
 };
 
-char response[10];
+char response[6];
 
 void printStudents(struct Student);
 void addStudent(struct Student);
 void deleteStudent(struct Student);
-void quit(struct Student);
+void quitStudentlist();
 
 vector<Student*>* studentlist;
 
-void addStudent(struct Student) {
+void addStudent() {
   cout << "Test add" << endl;
-  Student student;
-  cout << "Enter student first name" << endl;
-  cin >> student.firstname;
-  cout << "Enter student last name" << endl;
-  cin >> student.lastname;
-  cout << "Enter student ID" << endl;
-  cin >> student.id;
-  cout << "Enter student GPA" << endl;
-  cin >> student.gpa;
+  //Student student;
+  //cout << "Enter student first name" << endl;
+  //cin >> student.firstname;
+  //cout << "Enter student last name" << endl;
+  //cin >> student.lastname;
+  //cout << "Enter student ID" << endl;
+  //cin >> student.id;
+  //cout << "Enter student GPA" << endl;
+  //cin >> student.gpa;
 }
+
+void printStudents() {
+  cout << "Test print" << endl;
+  //Student student;
+  //cout << student.firstname << student.lastname << " ID:" << student.id << " GPA:" << student.gpa << endl;
+}
+
 
 int main() {
   do {
   cout << "Enter ADD, PRINT, DELETE, or QUIT" << endl;
   cin >> response;
   }
-  while (response == "ADD"); {
-    cout << "Adding student" << endl;
-    addStudent();
+  
+  while (response == "ADD" && response == "PRINT" && response == "DELETE" && response == "QUIT"); {
+    cout << "Entered loop" << endl;
+    if (response == "ADD") {
+      cout << "Adding loop running" << endl;
+      addStudent();
+    }
+    if (response == "PRINT") {
+      cout << "Printing loop running" << endl;
+      printStudents();
+    }
+    if (response == "DELETE") {
+      cout << "Deleting loop running" << endl;
+    }
+    if (response == "QUIT") {
+      cout << "Quitting" << endl;
+      return 0;
+    }
+    else {
+      cout << "Invalid input" << endl;
+    }
   }
   
-  if (response == "PRINT") {
-    cout << "Printing students" << endl;
-    printStudents();
-    }
-  
-   if (response == "DELETE") {
-   cout << "Deleting student" << endl;
-    }
-
-   if (response == "QUIT") {
-     cout << "Quiting" << endl;
-   }
+    
    vector <Student*>* studentlist = new vector<Student*>();
 }
-//for ADD
-//void addStudent(struct Student) {
-  //cout << "Test Add" << endl;
-  //Student student;
-   //cout << "Enter student first name" << endl;
-   //cin >> student.firstname;
-   //cout << "Enter student last name" << endl;
-   //cin >> student.lastname;
-   //cout << "Enter student ID" << endl;
-   //cin >> student.id;
-   //cout << "Enter student GPA" << endl;
-   // cin >> student.gpa;
-   //}
-
-  //for PRINT
-  void printStudents(struct Student) {
-  Student student;
-  cout << student.firstname << " " << student.lastname <<" ID:" << student.id << " GPA:";
-  cout << student.gpa << endl;
-  }
 
