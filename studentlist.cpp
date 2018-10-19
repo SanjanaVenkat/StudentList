3d40565b67f1cdf30653134f9cd970b7d90f9298
SanjanaVenkat <39311548+SanjanaVenkat@users.noreply.github.com>
2018-10-18 20:35:29 -0700

    Add files via upload
    Add and print loops almost completely working

--- studentlist.cpp
+++ studentlist.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-char response;
+char response[10];
 
 struct Student {
   char firstname[30];
@@ -20,50 +20,52 @@ void quitStudentlist();
 
 vector<Student*>* studentlist;
 
-void addStudent() {
+void addStudent(vector <Student*>* studentlist) {
   //cout << "Test add" << endl;
-  Student student;
+  Student* student = new Student();
   cout << "Enter student first name" << endl;
-  cin >> student.firstname;
+  cin >> student->firstname;
   cout << "Enter student last name" << endl;
-  cin >> student.lastname;
+  cin >> student->lastname;
   cout << "Enter student ID" << endl;
-  cin >> student.id;
+  cin >> student->id;
   cout << "Enter student GPA" << endl;
-  cin >> student.gpa;
-  cout << student.firstname << " " << student.lastname << ", " <<
- student.id << ", " << student.gpa << endl;
-  studentlist.push_back(student);
+  cin >> student->gpa;
+  cout << student->firstname << " " << student->lastname << ", " <<
+  student->id << ", " << student->gpa << endl;
+  studentlist->push_back(student);
 }
 
-void printStudents() {
-  cout << "Test print" << endl;
-  cout << studentlist << endl;
-
+void printStudents(vector <Student*>* studentlist) {
+  //cout << "Test print" << endl;
+  for (int i = 0; i < studentlist->size(); i++) {
+    Student* s = (*studentlist)[i];
+    cout << s->firstname << " " << s->lastname << ", " << s->id << ", " << s->gpa <<endl;
+  }
 }
 
 
 int main() {
-  //  cout << "Enter A, P, D, or Q" << endl;
-
-  
-  //if (response == "A") {
-      cout << "Adding loop running" << endl;
-      addStudent();
-      //  }
-      //if (response == "P") {
-      cout << "Printing loop running" << endl;
-      printStudents();
-      //}
-      //if (response == "D") {
+  vector <Student*>* studentlist = new vector<Student*>();
+  while(1) {
+      cout << "Enter ADD, PRINT, DELETE, or QUIT" << endl;
+      cin >> response;
+      while (response[0] == 'A' && response[1] == 'D' && response[2] == 'D') {
+	cout << "Adding" << endl;
+	addStudent(studentlist);
+	cout << "Enter ADD, PRINT, DELETE, or QUIT" << endl;
+      cin >> response;
+	}
+      while (response[0] == 'P' && response[1] == 'R' && response[2] == 'I' && response[3] == 'N' && response[4] == 'T') {
+      printStudents(studentlist);
+cout << "Enter ADD, PRINT, DELETE, or QUIT" << endl;
+ cin >> response;
+      }
       cout << "Deleting loop running" << endl;
-      //}
-      //if (response == "Q") {
       cout << "Quitting" << endl;
       return 0;
-      //}
-  
+  }
     
-   vector <Student*>* studentlist = new vector<Student*>();
+
 }
 
